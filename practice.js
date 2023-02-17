cdbaff28ed48331eac28f5817c09c23ef7ad8994
shivank111chauhan <shivank111chauhan@gmail.com>
2023-02-17 18:27:24 +0530

    Created add and sub with redux

--- practice.js
+++ practice.js
@@ -1,13 +1,12 @@
-// let arr = [1, 2, 3, 4, 5];
-// let sum = 0;
-// for (let i = 0; i < arr.length; i++) {
-//   sum = sum + arr[i];
-// }
-// console.log(sum);
+// const initialState = {
+//   uid: '',
+//   username: '',
+//   isUserLogin: false,
+//   hobby: [],
+// };
 
-let arr = [1, 2, 3, 4, 5];
-const output = arr.reduce((sum, currentvalue) => {
-  return sum + currentvalue;
-});
+const userReducer = (initialState, action) => {
+  console.log(action.type);
+};
 
-console.log(output);
+userReducer({type: 'addition'});
